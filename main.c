253d3f939eaf283b7464cb6dfa467a51a3f06a84
IbrahimMhSaeed <ibrahimMhSaeed.com>
2024-01-06 10:25:47 +0200

    adding main file and main header file

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - simple shell program
+ * Return: 0 Always
+ */
+
+int main()
+{
+	// loop of the shell
+	while (1)
+	{
+		//show prompt
+		//getinput + tokenizing input
+		//search in PATH
+		
+	}
+
+}
