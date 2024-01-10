407bfa43ec5bae80472f84413dea25ca68d2386c
HendZaineddin <hendzad.32@gmail.com>
2024-01-10 10:05:28 -0800

    Edit _exit file

--- _exit.c
+++ _exit.c
@@ -1,28 +1,38 @@
 #include "main.h"
 
 #define MAX_INPUT_SIZE 1024
-
-void _exit() {
-    printf("Exiting the shell...\n");
-    exit(0);
+/**
+ *_exit : exit function to exit the shell
+ *main :Entry oint.
+ *Return: 0.
+ */
+void _exit(void)
+{
+	printf("Exiting the shell...\n");
+	exit(0);
 }
 
-int main() {
-    char input[MAX_INPUT_SIZE];
+int main(void)
+{
+	char input[MAX_INPUT_SIZE];
 
-    while (1) {
-        printf("SimpleShell> ");
+	while (1)
+	{
+	printf("SimpleShell> ");
 
-        fgets(input, MAX_INPUT_SIZE, stdin);
+	fgets(input, MAX_INPUT_SIZE, stdin);
 
-        input[strcspn(input, "\n")] = '\0';
+	input[strcspn(input, "\n")] = '\0';
 
-        if (strcmp(input, "exit") == 0) {
-            _exit();
-        } else {
-            printf("Command not recognized: %s\n", input);
-        }
-    }
+		if (strcmp(input, "exit") == 0)
+		{
+			_exit();
+		}
+		else
+		{
+			printf("Command not recognized: %s\n", input);
+		}
+	}
 
-    return 0;
+	return (0);
 }
