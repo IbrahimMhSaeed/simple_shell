a8eec6ce723c02bd27dc729a6edc9d3272456a75
IbrahimMhSaeed <ibrahimmhsaeed@gmail.com>
2024-01-11 02:42:38 +0200

    Add non-interactive mode

--- main.c
+++ main.c
@@ -14,16 +14,26 @@ int main(int argc, char *argv[], char **env)
 	char **token;
 	char **path;
 
-	/**
-	 * if (argc > 1)
-	 * {
-	 * command = argv[1];
-	 * token = command_tokenization(command);
-	 * execute_arg(token);
-	 * }
-	 */
 
 	path = tokenize_path(env);
+
+	command = get_input();
+
+	if (command != NULL)
+	{
+		token = command_tokenization(command, path);
+	
+		if (token != NULL)
+		{
+			if (_strcmp(token[0], "exit") == 1)
+				exit(0);
+			execute(token, env);
+		}
+
+		return (0);
+	}
+
+	return (0);
 
 	(void) argc;
 	(void) argv;
